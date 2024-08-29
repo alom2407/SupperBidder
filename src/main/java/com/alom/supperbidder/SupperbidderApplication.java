10f032358428b1821452bef59909fc05189f96e0
alom <alom2407@gmail.com>
2024-08-30 00:53:44 +0530

    first add

---
+++ src/main/java/com/alom/supperbidder/SupperbidderApplication.java
@@ -0,0 +1,13 @@
+package com.alom.supperbidder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupperbidderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupperbidderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alom/supperbidder/SupperbidderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alom.supperbidder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupperbidderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
