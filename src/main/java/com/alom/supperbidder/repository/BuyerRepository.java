304c7abdccb75047735bac58693d85fc744f7586
alom <alom2407@gmail.com>
2024-09-05 22:07:55 +0530

    adding new buyer repository

--- src/main/java/com/alom/supperbidder/repository/BuyerRepository.java
+++ src/main/java/com/alom/supperbidder/repository/BuyerRepository.java
@@ -1,4 +1,14 @@
 package com.alom.supperbidder.repository;
 
+import com.alom.supperbidder.dto.Buyer;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class BuyerRepository {
+    Map<String, Buyer> buyerMap;
+
+    public BuyerRepository() {
+        this.buyerMap = new HashMap<>();
+    }
 }
